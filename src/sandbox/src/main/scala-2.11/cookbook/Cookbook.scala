24419aa77de73db4b3868aa88673f2a41b3b5ea6
Prashanth Janagama <janagama.prashanth@gmail.com>
2015-06-05 18:27:11 -0400

    Singleton

--- src/sandbox/src/main/scala-2.11/cookbook/Cookbook.scala
+++ src/sandbox/src/main/scala-2.11/cookbook/Cookbook.scala
@@ -3,16 +3,35 @@
  */
 package cookbook
 
-class Recipe(
+class Recipe private( //constructor is private
     val ingredients: List[String] = List.empty,
     val directions: List[String] = List.empty){
   println("Ingredients: "+ingredients)
   println("Directions: "+ directions)
 } 
 
+object Recipe {
+  def make(ingredients: List[String], directions: List[String]):Recipe = 
+    new Recipe(ingredients, directions)
+}
 
+/*
 class Cookbook(val recipes: Map[String, Recipe]) {
   println("Recipes: "+recipes)
   //auxillary constuctor
   def this() = this(Map.empty)
+}*/
+
+
+object Cookbook{
+  val pbj =  Recipe.make(
+     List("peanut butter", "jelly", "bread"),
+     List("put the peanut butter and jelly on the bread"))
+  
+  val baconPancakes = Recipe.make(
+     List("backon", "pancackes"),
+     List("take some bacon and put it in a pancake"))
+  
+  
+  
 }
